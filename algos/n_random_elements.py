fce9f7bed761f6f5c38bbc24dcef91821b677e27
AminMal <amin.malekloo@yahoo.com>
2020-12-04 02:28:08 +0330

    n random elements added

---
+++ algos/n_random_elements.py
@@ -0,0 +1,17 @@
+import random
+
+
+def random_elements(n: int, cap: int):
+
+    def iterate(numbers_to_go: int = n, acc: [int] = None):
+        if acc is None:
+            acc = []
+        if numbers_to_go == 0:
+            return acc + [random.randint(0, cap)]
+        else:
+            return iterate(numbers_to_go - 1, acc + [random.randint(0, cap)])
+
+    return iterate()
+
+
+print(random_elements(6, 49))
